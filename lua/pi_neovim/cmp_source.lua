0ced2023f518b4e4a0d3c677b7e3328f3ff25532
heavysudo <heavysudo@proton.me>
2026-08-13 17:59:16 -0400

    fix cmp config error

--- lua/pi_neovim/cmp_source.lua
+++ lua/pi_neovim/cmp_source.lua
@@ -3,13 +3,35 @@
 
 local M = {}
 
--- Required by nvim-cmp
+-- Create a new source instance
+function M.new()
+  local source = {}
+
+  -- Required by nvim-cmp: get trigger characters
+  source.get_trigger_characters = function()
+    return { "." }
+  end
+
+  -- Required by nvim-cmp: complete function
+  source.complete = function(self, params, callback)
+    -- TODO: Implement completion request
+    callback({})
+  end
+
+  -- Optional: resolve completion items
+  source.resolve = function(self, completion_item, callback)
+    callback(completion_item)
+  end
+
+  return source
+end
+
+-- For backward compatibility, also expose as module functions
 M.get_trigger_characters = function()
-  return { "." } -- example
+  return { "." }
 end
 
 M.complete = function(self, params, callback)
-  -- TODO: Implement completion request
   callback({})
 end
 
